d337aae89d610893d7e3d6c17a0087ce62fe09ae
cncyan <cncyan@163.com>
2017-03-09 21:23:47 +0800

    CSmarket

---
+++ CSmarket/js/index.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Administrator on 2017-03-09.
+ */
+$(document).ready(function(){
+    $('#featured-area ul').roundabout({
+        easing: 'easeOutInCirc',
+        duration: 600,
+        autoplay:true,
+        autoplayDuration: 3000,
+        autoplayPauseOnHover: true,
+        minScale: 0.7,
+        btnPrev: ".ban_r_btn", // 右按钮
+        btnNext: ".ban_l_btn" // 左按钮
+    });
+
+    //粒子背景
+    $('#particles').particleground({
+        dotColor: '#9d9d9d',
+        lineColor: '#999999'
+    });
+    //导航栏固定
+    $(window).scroll(function(){
+        var top=$(window).scrollTop();
+        if(top>=502){
+            $("#headertop").addClass("fix");
+            $("#headertop").css({color:"red",top:0});
+        }else{
+            $("#headertop").removeClass("fix");}
+    });
+
+});
